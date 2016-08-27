a23380a53e15c32abd6d5bae5caf669f15a9e86b
j7an <jiatan24@gmail.com>
2016-08-27 03:20:26 +0000

    add ex 1.2

---
+++ chap-1/ex1.2/DisplayFiveMessages.java
@@ -0,0 +1,11 @@
+/*
+Write a program that displays Welcome to Java five times.
+*/
+
+public class DisplayFiveMessages {
+  public static void main(String[] args) {
+    for (int x = 0; x < 5; x++) {
+      System.out.println("Welcome to Java");      
+    }
+  }
+}
