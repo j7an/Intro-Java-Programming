caca6a1fe3a00c3228743367565deec8ea9d0530
j7an <jiatan24@gmail.com>
2016-08-27 04:14:31 +0000

    add ex 1.3

---
+++ chap-1/ex1.3/DisplayPattern.java
@@ -0,0 +1,36 @@
+/*
+Write a program that displays the following pattern:
+
+    J    A    V     V   A
+    J   A A    V   V   A A
+J   J  AAAAA    V V   AAAAA
+ J J  A     A    V   A     A
+*/
+
+public class DisplayPattern {
+  public static void main(String[] args) {
+    // Row 1
+    System.out.print("    J");
+    System.out.print("    A");
+    System.out.print("    V     V");
+    System.out.println("   A");
+    
+    // Row 2
+    System.out.print("    J");
+    System.out.print("   A A");
+    System.out.print("    V   V");
+    System.out.println("   A A");
+    
+    // Row 3
+    System.out.print("J   J");
+    System.out.print("  AAAAA");
+    System.out.print("    V V");
+    System.out.println("   AAAAA");
+    
+    // Row 4
+    System.out.print(" J J");
+    System.out.print("  A     A");
+    System.out.print("    V");
+    System.out.println("   A     A");
+  }
+}
