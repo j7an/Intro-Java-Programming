85ddaa35030408ac25b92f30f8b6aa7d2149ed63
j7an <jiatan24@gmail.com>
2016-08-27 15:05:44 +0000

    Add ex1.6

---
+++ chap-1/ex1.6/SummationSeries.java
@@ -0,0 +1,10 @@
+/*
+Write a program that displays the result of
+ 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9.
+*/
+
+public class SummationSeries {
+  public static void main(String[] args) {
+    System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+  }
+}
