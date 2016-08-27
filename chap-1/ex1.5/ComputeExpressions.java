021b96e04ddb60fffb021f0e8c704da5314e27b3
j7an <jiatan24@gmail.com>
2016-08-27 05:33:15 +0000

    add ex 1.5

---
+++ chap-1/ex1.5/ComputeExpressions.java
@@ -0,0 +1,10 @@
+/*
+Write a program that displays the result of 9.5 * 4.5 - 2.5 * 3.
+45.5 - 3.5
+*/
+
+public class ComputeExpressions {
+  public static void main(String[] args) {
+    System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+  }
+}
