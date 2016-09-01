e3faa57b9a96a8df9d2ec20775f03d3e09525f9e
j7an <jiatan24@gmail.com>
2016-09-01 22:33:54 +0000

    add ex1.12

---
+++ chap-1/ex1.12/AverageSpeedKilometers.java
@@ -0,0 +1,19 @@
+/*
+Assume a runner runs 24 miles in 1 hour, 40 minutes, and 35 seconds. Write a
+program that displays the average speed in kilometers per hour. (Note that 1
+mile is 1.6 kilometers.)
+*/
+
+public class AverageSpeedKilometers {
+  public static void main(String[] args) {
+    int miles = 24;
+    double kilometers = miles * 1.6;
+    double hours = 1;
+    double kpm;
+    
+    hours = hours + (40 / 60) + (35 / 60 / 60);
+    kpm = kilometers / hours;
+    
+    System.out.println("Kilometers per hour: " + kpm);
+  }
+}
