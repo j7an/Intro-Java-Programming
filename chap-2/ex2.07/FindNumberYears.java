5286254f9acdbd86feadb6c2905dcc6b4f2dc599
j7an <jiatan24@gmail.com>
2016-09-02 04:12:40 +0000

    add ex2.07

---
+++ chap-2/ex2.07/FindNumberYears.java
@@ -0,0 +1,24 @@
+/* Write a program that prompts the user to enter the minutes (e.g., 1 billion),
+and displays the number of years and days for the min- utes. For simplicity,
+assume a year has 365 days.
+*/
+
+import java.util.Scanner;
+
+public class FindNumberYears {
+  public static void main(String[] args) {
+    int minutes = 0,
+      year = 0,
+      days = 0,
+      daysRemaining = 0;
+    
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter the number of minutes: ");
+    minutes = input.nextInt();
+    
+    days = minutes / 60 / 24;
+    year = days / 365;
+    daysRemaining = days % 365;
+    System.out.println(minutes + " minutes is approximately " + year + " years and " + daysRemaining + " days");
+  }
+}
