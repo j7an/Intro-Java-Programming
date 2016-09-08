b15af1a03cc8e0f84f31e08a24e97390649ca028
j7an <jiatan24@gmail.com>
2016-09-08 21:13:29 +0000

    add ex2.14

---
+++ chap-2/ex2.14/ComputingBMI.java
@@ -0,0 +1,23 @@
+/* Body Mass Index (BMI) is a measure of health on weight. It can be calculated
+by taking your weight in kilograms and dividing by the square of your height in
+meters. Write a program that prompts the user to enter a weight in pounds and
+height in inches and displays the BMI. Note that one pound is 0.45359237
+kilograms and one inch is 0.0254 meters.
+*/
+
+import java.util.Scanner;
+
+public class ComputingBMI {
+  public static void main(String[] args) {
+    double weightPounds,
+      heightInches;
+      
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter weight in pounds: ");
+    weightPounds = input.nextDouble();
+    System.out.print("Enter height in inches: ");
+    heightInches = input.nextDouble();
+    
+    System.out.println("BMI is " + (weightPounds * 0.45359237) / Math.pow((heightInches * 0.0254), 2));
+  }
+}
