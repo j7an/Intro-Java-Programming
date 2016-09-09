bc0026793538c8cb2c11a98daef29a862f0aec3c
j7an <jiatan24@gmail.com>
2016-09-09 01:32:13 +0000

    add ex2.18

---
+++ chap-2/ex2.18/PrintTable.java
@@ -0,0 +1,24 @@
+/* Write a program that displays the following table. Cast floating- point
+numbers into integers.
+
+a b pow(a, b)
+1 2 1
+2 3 8
+3 4 81
+4 5 1024
+5 6 15625
+*/
+
+public class PrintTable {
+  public static void main(String[] args) {
+    int a = 1,
+      b = 2,
+      pow = 0;
+    
+    System.out.println("a b pow(a, b)");
+    for (; a < 6; a++, b++) {
+      pow = (int) Math.pow(a, b);
+      System.out.println(a + " " + b + " " + pow);
+    }
+  }
+}
