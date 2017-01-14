cf8a7c4eac514be8f682a72acdb46e583da8e316
j7an <jiatan24@gmail.com>
2017-01-14 11:08:03 -0800

    random point

---
+++ chap-3/ex3.16/Ex3_16_RandomPoint.java
@@ -0,0 +1,21 @@
+/* 3.16 (Random point) Write a program that displays a random coordinate in a rectangle. The rectangle is centered at
+ * (0, 0) with width 100 and height 200.
+ */
+
+package chap_3;
+
+public class Ex3_16_RandomPoint {
+
+  public static void main(String[] args) {
+    int widthMin = -50;
+    int widthMax = 50;
+    int heightMin = -100;
+    int heightMax = 100;
+
+    int widthRand = (int) (Math.random() * ((widthMax - widthMin) + 1)) + widthMin;
+    int heightRand = (int) (Math.random() * ((heightMax - heightMin) + 1)) + heightMin;
+
+    System.out.println("Random point in rectangle is: (" + widthRand + ", " + heightRand + ")");
+  }
+
+}
