0f34a3a7c8a164b489c115f1d31933fa03b255f8
j7an <jiatan24@gmail.com>
2017-01-25 23:30:05 -0800

    Sentinel Value

---
+++ src/chap_05/Listing_5_05_SentinelValue.java
@@ -0,0 +1,28 @@
+package chap_05;
+
+import java.util.Scanner;
+
+public class Listing_5_05_SentinelValue {
+
+  public static void main(String[] args) {
+    // Create a Scanner
+    Scanner input = new Scanner(System.in);
+    
+    // Read an initial data
+    System.out.print("Enter an integer (the input ends if it is 0): ");
+    int data = input.nextInt();
+    
+    // Keep reading data util the input is 0
+    int sum = 0;
+    while (data != 0) {
+      sum += data;
+      
+      // Read the next data
+      System.out.print("Enter an integer (the input ends if it is 0): ");
+      data = input.nextInt();
+    }
+
+    System.out.println("The sum is " + sum);
+  }
+
+}
