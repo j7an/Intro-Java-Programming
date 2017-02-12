6a7e9b3e679c6b8f5a81eefbe4a579992b662ca0
j7an <jiatan24@gmail.com>
2017-02-12 12:02:33 -0800

    Calculator

---
+++ src/chap_07/Listing_7_09_Calculator.java
@@ -0,0 +1,30 @@
+package chap_07;
+
+public class Listing_7_09_Calculator {
+
+  public static void main(String[] args) {
+    if (args.length != 3) {
+      System.out.println("Usage: java Calculator operand1 operator operand2");
+      System.exit(0);
+    }
+
+    int result = 0;
+
+    switch (args[1].charAt(0)) {
+    case '+':
+      result = Integer.parseInt(args[0]) + Integer.parseInt(args[2]);
+      break;
+    case '-':
+      result = Integer.parseInt(args[0]) - Integer.parseInt(args[2]);
+      break;
+    case '.':
+      result = Integer.parseInt(args[0]) * Integer.parseInt(args[2]);
+      break;
+    case '/':
+      result = Integer.parseInt(args[0]) * Integer.parseInt(args[2]);
+    }
+    
+    System.out.println(args[0] + ' ' + args[1] + ' ' + args[2] + " = " + result);
+  }
+
+}
