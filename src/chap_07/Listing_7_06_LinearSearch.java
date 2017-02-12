543ae91972d8cedd634c6b703700f279951837a4
j7an <jiatan24@gmail.com>
2017-02-12 11:11:47 -0800

    test

--- src/chap_07/Listing_7_06_LinearSearch.java
+++ src/chap_07/Listing_7_06_LinearSearch.java
@@ -10,4 +10,12 @@ public class Listing_7_06_LinearSearch {
     return -1;
   }
 
+  public static void main(String[] args) {
+    int[] list = {1, 4, 4, 2, 5, -3, 6, 2};
+    int i = linearSearch(list, 4);
+    int j = linearSearch(list, -4);
+    int k = linearSearch(list, -3);
+    
+    System.out.println(i + " " + j + " " + k);
+  }
 }
