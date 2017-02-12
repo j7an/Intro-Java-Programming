3865a031c11618adb40627b5187ae2d182c099c0
j7an <jiatan24@gmail.com>
2017-02-12 11:18:08 -0800

    test

--- src/chap_07/Listing_7_07_BinarySearch.java
+++ src/chap_07/Listing_7_07_BinarySearch.java
@@ -16,4 +16,13 @@ public class Listing_7_07_BinarySearch {
     }
     return -low - 1;
   }
+  
+  public static void main(String[] args) {
+    int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+    System.out.println(binarySearch(list, 2));
+    System.out.println(binarySearch(list, 11));
+    System.out.println(binarySearch(list, 12));
+    System.out.println(binarySearch(list, 1));
+    System.out.println(binarySearch(list, 3));
+  }
 }
