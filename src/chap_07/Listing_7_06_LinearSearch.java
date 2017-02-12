01086e20a70244c9413721f1487338c572fbab86
j7an <jiatan24@gmail.com>
2017-02-12 10:46:02 -0800

    LinearSearch

---
+++ src/chap_07/Listing_7_06_LinearSearch.java
@@ -0,0 +1,13 @@
+package chap_07;
+
+public class Listing_7_06_LinearSearch {
+
+  public static int linearSearch(int[] list, int key) {
+    for (int i = 0; i < list.length; i++) {
+      if (key == list[i])
+        return i;
+    }
+    return -1;
+  }
+
+}
